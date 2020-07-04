b67db44d457d9bac85f7d0a2072c84f4e7bf6a88
sarstuff <46381080+sarstuff@users.noreply.github.com>
2020-07-04 18:48:21 +0100

    updating to new

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { GenerateSW, InjectManifest } = require("workbox-webpack-plugin");
 
-module.exports = function(_env, argv) {
+module.exports = function (_env, argv) {
   const isProduction = argv.mode === "production";
   const isDevelopment = !isProduction;
 
@@ -13,30 +16,36 @@ module.exports = function(_env, argv) {
     devtool: isDevelopment && "cheap-module-source-map",
     entry: "./src/index.js",
     output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "assets/js/[name].[contenthash:8].js",
-      publicPath: "/"
+      path: path.resolve(__dirname, "build"),
+      filename: "assets/js/[name].js",
+      // filename: "assets/js/[name].[contenthash:8].js",
+      publicPath: "/",
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+      alias: {
+        "react-dom": "@hot-loader/react-dom",
+      },
     },
     module: {
       rules: [
         {
-          test: /\.jsx?$/,
+          test: /\.js$/,
+          // loader: "babel-loader",
           exclude: /node_modules/,
           use: {
             loader: "babel-loader",
             options: {
               cacheDirectory: true,
               cacheCompression: false,
-              envName: isProduction ? "production" : "development"
-            }
-          }
+              envName: isProduction ? "production" : "development",
+            },
+          },
         },
+
         {
           test: /\.css$/,
-          use: [
-            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
-            "css-loader"
-          ]
+          use: [isProduction ? MiniCssExtractPlugin.loader : "style-loader", "css-loader"],
         },
         {
           test: /\.(png|jpg|gif)$/i,
@@ -44,61 +53,84 @@ module.exports = function(_env, argv) {
             loader: "url-loader",
             options: {
               limit: 8192,
-              name: "static/media/[name].[hash:8].[ext]"
-            }
-          }
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
         },
         {
           test: /\.svg$/,
-          use: ["@svgr/webpack"]
+          use: ["@svgr/webpack"],
         },
         {
           test: /\.(eot|otf|ttf|woff|woff2)$/,
           loader: require.resolve("file-loader"),
           options: {
-            name: "static/media/[name].[hash:8].[ext]"
-          }
-        }
-      ]
-    },
-    resolve: {
-      extensions: [".js", ".jsx"]
+            name: "static/media/[name].[hash:8].[ext]",
+          },
+        },
+      ],
     },
     plugins: [
-      isProduction &&
-        new MiniCssExtractPlugin({
-          filename: "assets/css/[name].[contenthash:8].css",
-          chunkFilename: "assets/css/[name].[contenthash:8].chunk.css"
-        }),
+      isProduction && new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "assets/css/[name].css",
+        chunkFilename: "assets/css/[name].[contenthash:8].chunk.css",
+      }),
       new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "public/index.html"),
-        inject: true
+        template: path.resolve(__dirname, "src/index.html"),
+        inject: true,
       }),
       new webpack.DefinePlugin({
-        "process.env.NODE_ENV": JSON.stringify(
-          isProduction ? "production" : "development"
-        )
-      })
+        "process.env.NODE_ENV": JSON.stringify(isProduction ? "production" : "development"),
+      }),
+      // new GenerateSW({
+      //   include: [/\.html$/, /\.js$/, /\.css$/],
+      //   exclude: ["/node_modules"],
+      // }),
+      new InjectManifest({
+        swSrc: "./src/sw-config.js",
+        swDest: "sw.js",
+        maximumFileSizeToCacheInBytes: 9961472,
+        // include: ["**/*.{html,js,css}"],
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "src", "assets/images"),
+            to: path.resolve(__dirname, "build", "assets/images"),
+          },
+          {
+            from: path.resolve(__dirname, "src", "manifest.json"),
+            to: path.resolve(__dirname, "build", "manifest.json"),
+          },
+
+          {
+            from: path.resolve(__dirname, "src", "favicon.ico"),
+            to: path.resolve(__dirname, "build", "favicon.ico"),
+          },
+        ],
+      }),
     ].filter(Boolean),
+
     optimization: {
       minimize: isProduction,
       minimizer: [
         new TerserWebpackPlugin({
           terserOptions: {
             compress: {
-              comparisons: false
+              comparisons: false,
             },
             mangle: {
-              safari10: true
+              safari10: true,
             },
             output: {
               comments: false,
-              ascii_only: true
+              ascii_only: true,
             },
-            warnings: false
-          }
+            warnings: false,
+          },
         }),
-        new OptimizeCssAssetsPlugin()
+        new OptimizeCssAssetsPlugin(),
       ],
       splitChunks: {
         chunks: "all",
@@ -109,25 +141,25 @@ module.exports = function(_env, argv) {
           vendors: {
             test: /[\\/]node_modules[\\/]/,
             name(module, chunks, cacheGroupKey) {
-              const packageName = module.context.match(
-                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-              )[1];
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
               return `${cacheGroupKey}.${packageName.replace("@", "")}`;
-            }
+            },
           },
           common: {
             minChunks: 2,
-            priority: -10
-          }
-        }
+            priority: -10,
+          },
+        },
       },
-      runtimeChunk: "single"
+      runtimeChunk: "single",
     },
     devServer: {
       compress: true,
       historyApiFallback: true,
       open: true,
-      overlay: true
-    }
+      overlay: true,
+      port: 3000,
+      contentBase: "./src/",
+    },
   };
 };
